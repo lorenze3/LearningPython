779c24f4593525c519e2e0eba3cd6ef2b5edaa6c
lorenze3 <lorenze3@users.noreply.github.com>
2017-03-21 17:13:35 -0500

    Improved docstring

--- FileList2.py
+++ FileList2.py
@@ -1,16 +1,40 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 21 15:47:37 2017
 
-@author: lorete01
-"""
 import datetime
 import os
 
+
 def fileList(folder='c:\\', date=datetime.date.today()):
-    filesList=[]
-    for root,dir,fn in os.walk(folder):
+    """
+    Generates list of files from a folder modified after date
+    
+    Parameters
+    __________
+    folder: string
+        a path name to be used as root of the search
+    date: date
+        only files modified on or after this date will be kept in the list
+        default is today
+    Returns 
+    ________
+    filesList:dictionary
+     a dictionary where each filename by absolute path
+    is the key to the date modified value.
+    
+    Example
+    _______
+    > fileList(folder="c:\\temp", date=datetime.date(2017,3,21))
+    ["<first file>",
+    . . .          ,
+     "<last file>"]
+    
+    Test on a directory with few subfoldres
+    
+    @author: lorete01
+    """
+    filesList = []
+    for root, dir, fn in os.walk(folder):
         for file in fn:
-            #print(root + file)
-            filesList+=[root+file]
+            # print(root + file)
+            filesList += [root+file]
     return filesList

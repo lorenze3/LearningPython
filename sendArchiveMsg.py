791d6afa131313c6fb1b0a48e9a8295e2b65d359
lorenze3 <lorenze3@users.noreply.github.com>
2017-03-23 13:13:17 -0500

    Attachment now has filename listed

--- sendArchiveMsg.py
+++ sendArchiveMsg.py
@@ -4,21 +4,21 @@ Created on Wed Mar 22 16:05:20 2017
 
 @author: lorete01
 """
-
+## todo get file attachment to come through with a name
 import smtplib
 from email.message import EmailMessage
 
-def sendArchiveMsg(status='Success', body='Archiving ' +status +  
-                   '! Index File attached.',
-                   attachment='C:\\temp\\test\\list.txt'):
+def sendArchiveMsg(toaddr='lorenze3@gmail.com', status='Success',
+                   body='Archiving Index File Attached',
+                   attachment='C:\\temp\\list.txt'):
     msg = EmailMessage()
     msg.set_content(body)
     msg['Subject'] = 'Archiving Process' + status
     msg['From'] = 'autoreportingemail@gmail.com'
-    msg['To'] = 'ted.lorenzen@nielsen.com'
+    msg['To'] = toaddr
     with open(attachment) as att:
         att_data = att.read()
-    msg.add_attachment(att_data, maintype= text)
+    msg.add_attachment(att_data,filename=attachment)
     gmail = smtplib.SMTP('smtp.gmail.com', port=587)
     gmail.ehlo()
     gmail.starttls()
